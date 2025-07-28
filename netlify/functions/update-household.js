bda60021b6913ba29130c24d4cc8ea6fbf212f26
Changjoo Jeon <changjoojeon@gmail.com>
2025-07-28 15:19:59 -0400

    fix 2 for image

--- netlify/functions/update-household.js
+++ netlify/functions/update-household.js
@@ -21,8 +21,10 @@ exports.handler = async (event) => {
   }
 
   try {
+    console.log('Netlify Function: update-household - event.body:', event.body);
     const { coordinates, ...rest } = JSON.parse(event.body);
     const updatedHouseholdData = { ...rest, lat: coordinates?.lat, lng: coordinates?.lng };
+    console.log('Netlify Function: update-household - updatedHouseholdData:', updatedHouseholdData);
 
     const { data, error } = await supabaseAdmin.from('households').update(updatedHouseholdData).eq('householdId', householdId).select();
     if (error) throw error;
